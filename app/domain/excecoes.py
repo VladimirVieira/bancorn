2293f0c4255d37c7b6180661d7153f3d6ada8642
Pablo Gustavo Fernandes Maia <pablo.maia.704@ufrn.edu.br>
2025-05-18 23:47:05 -0300

    feat: adiciona excecoes customizadas

---
+++ app/domain/excecoes.py
@@ -0,0 +1,11 @@
+class ContaJaExiste(Exception):
+    def __init__(self, mensagem: str = 'Conta já existe') -> None:
+        super().__init__(mensagem)
+
+class ContaNaoEncontrada(Exception):
+    def __init__(self, mensagem: str = 'Conta não encontrada') -> None:
+        super().__init__(mensagem)
+
+class ValorTransferenciaInvalido(Exception):
+    def __init__(self, mensagem: str = 'O valor da transferência deve ser maior que zero') -> None:
+        super().__init__(mensagem)
