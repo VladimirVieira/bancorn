134c45bb430644791f8843d10b34c4b9a281cec6
Pablo Gustavo Fernandes Maia <pablo.maia.704@ufrn.edu.br>
2025-05-18 23:47:26 -0300

    refactor: ajusta metodos, tipagens e logica da classe AgenciaBancaria

--- app/domain/agencia_bancaria.py
+++ app/domain/agencia_bancaria.py
@@ -1,61 +1,30 @@
-from typing import List, Optional, Tuple
+from decimal import Decimal
+
 from .conta import Conta
+from .excecoes import ContaJaExiste, ContaNaoEncontrada, ValorTransferenciaInvalido
 
-class AgenciaBancaria(object):
-    
+class AgenciaBancaria:
     def __init__(self) -> None:
-        self.contas: List[Conta] = []
+        self.contas: list[Conta] = []
 
+    def criar_conta(self, numero: str) -> Conta:
+        if any(c.numero == numero for c in self.contas):
+            raise ContaJaExiste()
+        
+        nova_conta = Conta(numero)
+        self.contas.append(nova_conta)
+        
+        return nova_conta
 
-
-    def comparaConta(self, testeconta: int) -> bool:
-        for id in self.contas:
-            if id.numero == testeconta:
-                return True
-        return False
-
-    def cadastrarConta(self, numeroConta: int) -> bool:
-        if not self.comparaConta(numeroConta):
-            self.contas.append(Conta(numeroConta))
-            return True
-        return False
-
-
-    def consultarSaldo(self, numeroConta: int) -> Optional[Tuple[int, float]]:
-        for i in self.contas:
-            if i.numero == numeroConta:
-                return i.numero, i.saldo
-        return None
-
-
-
-    def atualizarCredito(self, numeroConta: int, valor: float) -> bool:
-        for i in self.contas:
-            if i.numero == numeroConta:
-                i.saldo+=valor
-                return True
-        return False
- 
-
-    def atualizarDebito(self, numeroConta: int, valor: float) -> bool:
-        for i in self.contas:
-            if i.numero == numeroConta:
-              i.saldo-=valor
-              return True
-        return False
-
-    def realizarTransferencia(self, contaorigem: int, contadestino: int, valor: float) -> bool:
-        origem: Optional[Conta] = None
-        destino: Optional[Conta] = None
-
+    def buscar_conta(self, numero: str) -> Conta:
         for conta in self.contas:
-            if conta.numero == contaorigem:
-                origem = conta
-            elif conta.numero == contadestino:
-                destino = conta
-
-        if origem and destino:
-            origem.escolherDebito(valor)
-            destino.escolherCredito(valor)
-            return True
-        return False
+            if conta.numero == numero:
+                return conta
+        raise ContaNaoEncontrada()
+    
+    def transferir(self, conta_origem: Conta, conta_destino: Conta, valor: Decimal) -> None:
+        if valor <= 0:
+            raise ValorTransferenciaInvalido
+        
+        conta_origem.debitar(valor)
+        conta_destino.creditar(valor)
