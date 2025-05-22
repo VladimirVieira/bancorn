4a105b4d10bacd9c42e9c319798676793d6539bc
Pablo Gustavo Fernandes Maia <pablo.maia.704@ufrn.edu.br>
2025-05-22 18:13:45 -0300

    refactor: aplica refatoracoes pontuais

--- app/domain/conta.py
+++ app/domain/conta.py
@@ -1,12 +1,14 @@
 from decimal import Decimal
-from .excecoes import SaldoInsuficienteError, ValorOperacaoInvalido
+
+from .excecoes import SaldoInsuficiente, ValorOperacaoInvalido
+
 
 class Conta:
     def __init__(self, numero: str) -> None:
         self.numero = numero
-        self._saldo = Decimal('0.00')
+        self._saldo = Decimal("0.00")
 
-     def creditar(self, valor: Decimal) -> None:
+    def creditar(self, valor: Decimal) -> None:
         if valor <= 0:
             raise ValorOperacaoInvalido()
         self._saldo += valor
@@ -15,15 +17,15 @@ class Conta:
         if valor <= 0:
             raise ValorOperacaoInvalido()
         if valor > self._saldo:
-            raise SaldoInsuficienteError()
+            raise SaldoInsuficiente()
         self._saldo -= valor
 
     @staticmethod
     def formatar_saldo(saldo: Decimal) -> str:
-        return f'R$ {saldo:.2f}'
+        return f"R$ {saldo:.2f}"
 
     def __str__(self) -> str:
-        return f'Conta {self.numero} - Saldo: {self.formatar_saldo(self._saldo)}'
-    
+        return f"Conta {self.numero} - Saldo: {self.formatar_saldo(self._saldo)}"
+
     def __hash__(self) -> int:
         return hash(self.numero)
