9d1b423a87a71adb4bcd5c5f7a4c6b048ef79607
Vlad Vieira <vladnascimento118@gmail.com>
2025-05-31 17:11:15 -0300

    feat: requerer saldo inicial ao realizar a criação da conta poupança. Closes #13

--- app/domain/conta_poupanca.py
+++ app/domain/conta_poupanca.py
@@ -6,6 +6,14 @@ from .excecoes import ValorOperacaoInvalido
 
 
 class ContaPoupanca(Conta):
+
+    def __init__(self, numero: str, saldo_inicial: Decimal):
+    	super().__init__(numero)
+        if saldo_inicial < 0:
+            raise ValorOperacaoInvalido("O saldo inicial é negativo e por isso a operação é inválida.")
+        self._saldo = saldo_inicial
+        
+        
     def render_juros(self, taxa_percentual: Decimal) -> None:
         if taxa_percentual <= 0 or taxa_percentual > 100:
             raise ValorOperacaoInvalido(
