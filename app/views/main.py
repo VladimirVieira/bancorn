0286faf090a8d0f566819ec4918d80c73fd1a0c5
Pablo Gustavo <34045117+Pablo1Gustavo@users.noreply.github.com>
2025-06-01 21:31:02 -0300

    Implementar Interface Visual Inicial (#18)
    Issue: #10

---
+++ app/views/main.py
@@ -0,0 +1,20 @@
+from textual.app import App
+
+from app.views.menus.menu_principal import MenuPrincipal
+
+
+class MenuApp(App):
+    TITLE = "BANCORN"
+    BINDINGS = [("escape", "voltar", "Voltar")]
+    CSS_PATH = "style.tcss"
+
+    def on_mount(self) -> None:
+        self.push_screen(MenuPrincipal())
+
+    def action_voltar(self) -> None:
+        if len(self.screen_stack) > 2:
+            self.pop_screen()
+
+
+if __name__ == "__main__":
+    MenuApp().run()
