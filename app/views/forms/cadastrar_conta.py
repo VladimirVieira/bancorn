c8d6c60afb2997b511797b52f952accd5115ff69
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2025-06-01 21:43:34 -0300

    feat: adiciona interacao na tela de cadastro de conta para solicitar saldo inicial de poupanca

--- app/views/forms/cadastrar_conta.py
+++ app/views/forms/cadastrar_conta.py
@@ -3,6 +3,7 @@ from textual.containers import Vertical
 from textual.screen import Screen
 from textual.widgets import Button, Footer, Header, Input, Select, Static
 
+from app.views.componentes.input_dinheiro import DinheiroInput
 
 class CadastrarConta(Screen):
     def compose(self) -> ComposeResult:
@@ -19,17 +20,28 @@ class CadastrarConta(Screen):
                 id="tipo_conta",
             ),
             Input(placeholder="Número da conta", id="numero_conta"),
+            Static("Valor inicial", id="valor_inicial_label"),
+            DinheiroInput(placeholder="Valor inicial", id="valor_inicial"),
             Button("Cadastrar", id="cadastrar"),
             Button("Voltar", id="voltar", classes="voltar"),
             id="form",
         )
         yield Footer()
 
+    def on_mount(self) -> None:
+        self.query_one("#valor_inicial").display = False
+        self.query_one("#valor_inicial_label").display = False
+
+    def on_select_changed(self, event: Select.Changed) -> None:
+        self.query_one("#valor_inicial").display = event.value == "poupanca"
+        self.query_one("#valor_inicial_label").display = event.value == "poupanca"
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
         match event.button.id:
             case "cadastrar":
                 tipo = self.query_one("#tipo_conta", Select).value
-                numero= self.query_one("#numero_conta", Input).value
-                print(f"Conta cadastrada: Tipo={tipo}, Nome={numero}")
+                numero = self.query_one("#numero_conta", Input).value
+                valor_inicial = self.query_one("#valor_inicial", Input).value
+                print(f"Conta cadastrada: Tipo={tipo}, Número={numero}, Valor inicial={valor_inicial}")
             case "voltar":
                 self.app.pop_screen()
