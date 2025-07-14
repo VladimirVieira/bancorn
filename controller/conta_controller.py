8c827096ddb1643cce181268bc68db5218b71d6d
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2025-07-14 19:51:26 -0300

    fix: corrige bug referente a creditar

--- controller/conta_controller.py
+++ controller/conta_controller.py
@@ -80,5 +80,6 @@ def render_juros(numero: str, dados: RenderJurosDTO) -> dict[str, str]:
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+
 def erro_sintaxe() -> None:
     return None
